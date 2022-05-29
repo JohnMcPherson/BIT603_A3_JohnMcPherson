12f431814325739d35857bd90fbc71677e5b6064
JohnMcPherson <john.mcpherson@xtra.co.nz>
2022-05-30 10:48:33 +1200

    Initial Commit

---
+++ app/src/main/java/nz/co/afleet/bit603_a3_johnmcpherson/MainActivity.java
@@ -0,0 +1,14 @@
+package nz.co.afleet.bit603_a3_johnmcpherson;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
