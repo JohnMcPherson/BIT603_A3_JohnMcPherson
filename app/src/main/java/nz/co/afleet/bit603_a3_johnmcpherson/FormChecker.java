c8119d1b04f689a99cc180ffa5c2a811c4d1b1a7
JohnMcPherson <john.mcpherson@xtra.co.nz>
2022-05-30 14:27:02 +1200

    Renamed Utilities to FormChecker - still does the same thing - determineErrorMessage - starting to make more generic (creates messages for 0 - n fields) - passing test for failed login message

---
+++ app/src/main/java/nz/co/afleet/bit603_a3_johnmcpherson/FormChecker.java
@@ -0,0 +1,111 @@
+package nz.co.afleet.bit603_a3_johnmcpherson;
+
+import android.content.Context;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class FormChecker {
+
+    private final Context context; // supports retrieval of strings from the string resources
+    private final int stringResourceInvalidCombination; //the message for when the user has filled in both fields, but the action failed. e.g. incorrect user/password combination
+    private final int stringResourceDetailsRequiredHeader; //the beginning of the message to indicate missing field(s) e.g. "Please enter your"
+
+    private final boolean actionSucceeded;
+    // true if the form has been submitted successfully, based on the supplied user data. e.g. User name and password have been validated
+        // false: if the combination is invalid; or the combination has not been tried (because of missing field[s])
+
+    private ArrayList<Integer> mandatoryFieldsNotPopulated = new ArrayList<>();
+
+    /**
+     *
+     * @param context
+     * @param stringResourceInvalidCombination
+     *@param stringResourceDetailsRequiredHeader
+     *@param fieldsFilledOrNotFilled
+     *     // the key refers to the name of the mandatory field, as defined in the string resource matching the key
+     *     // the value defines whether that field has been filled or not
+     *
+     *@param actionSucceeded
+     */
+    public FormChecker(
+                        Context context,
+                        int stringResourceInvalidCombination,
+                       int stringResourceDetailsRequiredHeader,
+                       HashMap<Integer, Boolean> fieldsFilledOrNotFilled,
+                       boolean actionSucceeded) {
+        this.context = context;
+        this.stringResourceInvalidCombination = stringResourceInvalidCombination;
+        this.stringResourceDetailsRequiredHeader = stringResourceDetailsRequiredHeader;
+        this.actionSucceeded = actionSucceeded;
+        initialiseFieldsNotPopulated(fieldsFilledOrNotFilled);
+    }
+
+    public static String determineErrorMessage(Context context,
+                                               int stringResourceInvalidCombination,
+                                               int stringResourceDetailsRequiredHeader,
+                                               HashMap<Integer, Boolean> fieldsFilledOrNotFilled,
+                                               boolean actionSucceeded) {
+        FormChecker formChecker = new FormChecker(
+                context,
+                stringResourceInvalidCombination,
+                stringResourceDetailsRequiredHeader,
+                fieldsFilledOrNotFilled,
+                actionSucceeded);
+
+        return formChecker.determineErrorMessage();
+    }
+
+    /**
+     * Generic method for returning an error message for a form with two mandatory fields.
+     *
+     * actionSucceeded should not be true if any of the mandatory fields are false;
+     * but this method does not check for that. The method will not fail, but the returned "" would not make sense
+     *
+     * @return an error message
+     */
+    private String determineErrorMessage() {
+        if (actionSucceeded) return ""; // clear the error message
+
+        // we don't have a successful action. If we have all fields filled in, there was a problem using them
+        if (allMandatoryFieldsPopulated()) return context.getString(stringResourceInvalidCombination);
+
+        // We are missing at least one field, so populate the error message with the "header" and the first missing field name
+        String nameOfFirstMissingField = getNameOfMissingField(context, 0);
+        String errorMessage = context.getString(stringResourceDetailsRequiredHeader)
+                + " " + nameOfFirstMissingField;
+
+/*
+        // add any fields between the first field and the last field (there may not be any)
+        // use commas as separators
+        for (int index = 1; index < mandatoryFieldsNotPopulated.size() - 1; index++) {
+            String nameOfNextField = getNameOfMissingField(context, index);
+            errorMessage += ", " + nameOfNextField;
+        }
+
+*/
+        if (mandatoryFieldsNotPopulated.size() > 1) { // we need to finish with "and [last field name]"
+            // add the name of the last field
+            String nameOfLastField = getNameOfMissingField(context, mandatoryFieldsNotPopulated.size() - 1);
+            errorMessage += " " + context.getString(R.string.and) + " " + nameOfLastField;
+        }
+        return errorMessage;
+    }
+
+    private String getNameOfMissingField(Context context, int index) {
+        Integer stringResourceIdentifier = mandatoryFieldsNotPopulated.get(index);
+        return context.getString(stringResourceIdentifier);
+    }
+
+    private void initialiseFieldsNotPopulated(HashMap<Integer, Boolean> fieldsFilledOrNotFilled) {
+        mandatoryFieldsNotPopulated = new ArrayList<>();
+        for (Map.Entry<Integer, Boolean> fieldFilledOrNotFilled : fieldsFilledOrNotFilled.entrySet()) {
+            if (!fieldFilledOrNotFilled.getValue()) mandatoryFieldsNotPopulated.add(fieldFilledOrNotFilled.getKey());
+        }
+    }
+
+    private boolean allMandatoryFieldsPopulated() {
+        return (mandatoryFieldsNotPopulated.isEmpty());
+    }
+}
--- app/src/test/java/nz/co/afleet/bit603_a3_johnmcpherson/IntegrationTest_ErrorMessages.java
+++ app/src/test/java/nz/co/afleet/bit603_a3_johnmcpherson/IntegrationTest_ErrorMessages.java
@@ -9,7 +9,9 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.RobolectricTestRunner;
 
-import static nz.co.afleet.bit603_a3_johnmcpherson.Utilities.determineErrorMessage;
+import java.util.HashMap;
+
+import static nz.co.afleet.bit603_a3_johnmcpherson.FormChecker.determineErrorMessage;
 import static org.junit.Assert.assertTrue;
 
 @RunWith(RobolectricTestRunner.class)
@@ -23,94 +25,39 @@ public class IntegrationTest_ErrorMessages {
 
     @Test
     public void testErrorMessages() {
+/*
         checkMessageForSuccess();
         checkMessageForBothFieldsMissing();
         checkMessageForFirstFieldMissing();
         checkMessageForSecondFieldMissing();
+*/
         checkMessageForActionFailed();
     }
 
-    private void checkMessageForSuccess() {
-        checkErrorMessage("",
-                true,
-                true,
-                R.string.error_incorrect_login,
-                R.string.login_details_required_header,
-                R.string.login_user_field_name,
-                R.string.login_password_field_name,
-                true
-        );
-
-    }
-
-    private void checkMessageForBothFieldsMissing() {
-        checkErrorMessage("Please enter your user name and password",
-                false,
-                false,
-                R.string.error_incorrect_login,
-                R.string.login_details_required_header,
-                R.string.login_user_field_name,
-                R.string.login_password_field_name,
-                false
-        );
-
-    }
-
-    private void checkMessageForFirstFieldMissing() {
-        checkErrorMessage("Please enter your user name",
-                false,
-                true,
-                R.string.error_incorrect_login,
-                R.string.login_details_required_header,
-                R.string.login_user_field_name,
-                R.string.login_password_field_name,
-                false
-        );
-
-    }
-
-    private void checkMessageForSecondFieldMissing() {
-        checkErrorMessage("Please enter your password",
-                true,
-                false,
-                R.string.error_incorrect_login,
-                R.string.login_details_required_header,
-                R.string.login_user_field_name,
-                R.string.login_password_field_name,
-                false
-        );
-    }
-
     private void checkMessageForActionFailed() {
+        HashMap<Integer, Boolean> fieldsFilledOrNotFilled = new HashMap<>();
+        fieldsFilledOrNotFilled.put(R.string.login_user_field_name, true);
+        fieldsFilledOrNotFilled.put(R.string.login_password_field_name, true);
 
         checkErrorMessage("Sorry. We did not recognise that user/password combination. Please try again",
-                true,
-                true,
                 R.string.error_incorrect_login,
                 R.string.login_details_required_header,
-                R.string.login_user_field_name,
-                R.string.login_password_field_name,
+                fieldsFilledOrNotFilled,
                 false
         );
     }
 
     private void checkErrorMessage(String expectedMessage,
-                                   boolean isFilledFirstField,
-                                   boolean isFilledSecondField,
                                    int stringResourceInvalidCombination,
                                    int stringResourceDetailsRequiredHeader,
-                                   int stringResourceNameOfFirstField,
-                                   int stringResourceNameOfSecondField,
+                                   HashMap<Integer, Boolean> fieldsFilledOrNotFilled,
                                    boolean actionSucceeded) {
 
         String actualMessage = determineErrorMessage(
                 application,
-                isFilledFirstField,
-                isFilledSecondField,
                 stringResourceInvalidCombination,
                 stringResourceDetailsRequiredHeader,
-                stringResourceNameOfFirstField,
-                stringResourceNameOfSecondField,
+                fieldsFilledOrNotFilled,
                 actionSucceeded
         );
 
