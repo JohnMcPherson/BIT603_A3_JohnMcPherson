defe2484407b87ec679a61f66b93fc50c83204f1
JohnMcPherson <john.mcpherson@xtra.co.nz>
2022-06-02 11:58:51 +1200

    navigation-drawer-experiment - add NavigationDrawerActivity (using AndroidStudio Wizard) - use Intent to go from MainActivity to NavigationDrawerActivity
    - menu not working

---
+++ app/src/main/java/nz/co/afleet/bit603_a3_johnmcpherson/ui/slideshow/SlideshowViewModel.java
@@ -0,0 +1,19 @@
+package nz.co.afleet.bit603_a3_johnmcpherson.ui.slideshow;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class SlideshowViewModel extends ViewModel {
+
+    private final MutableLiveData<String> mText;
+
+    public SlideshowViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is slideshow fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
