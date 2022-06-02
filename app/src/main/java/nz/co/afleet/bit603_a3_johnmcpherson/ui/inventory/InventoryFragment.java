65b66a561731f57cbe848cfd34d379d430d2d5de
JohnMcPherson <john.mcpherson@xtra.co.nz>
2022-06-02 18:11:39 +1200

    Refactor - Renaming to reflect the menu we want

--- app/src/main/java/nz/co/afleet/bit603_a3_johnmcpherson/ui/inventory/InventoryFragment.java
+++ app/src/main/java/nz/co/afleet/bit603_a3_johnmcpherson/ui/inventory/InventoryFragment.java
@@ -1,4 +1,4 @@
-package nz.co.afleet.bit603_a3_johnmcpherson.ui.home;
+package nz.co.afleet.bit603_a3_johnmcpherson.ui.inventory;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -8,22 +8,21 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
 
-import nz.co.afleet.bit603_a3_johnmcpherson.databinding.FragmentHomeBinding;
+import nz.co.afleet.bit603_a3_johnmcpherson.databinding.FragmentInventoryBinding;
 
-public class HomeFragment extends Fragment {
+public class InventoryFragment extends Fragment {
 
-    private FragmentHomeBinding binding;
+    private FragmentInventoryBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
-        binding = FragmentHomeBinding.inflate(inflater, container, false);
+        binding = FragmentInventoryBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textHome;
-        textView.setText("This is the Home Fragment");
+        final TextView textView = binding.textInventory;
+        textView.setText("This is the Inventory Fragment");
         return root;
     }
 
--- app/src/main/java/nz/co/afleet/bit603_a3_johnmcpherson/ui/users/UsersFragment.java
+++ app/src/main/java/nz/co/afleet/bit603_a3_johnmcpherson/ui/users/UsersFragment.java
@@ -1,4 +1,4 @@
-package nz.co.afleet.bit603_a3_johnmcpherson.ui.home;
+package nz.co.afleet.bit603_a3_johnmcpherson.ui.users;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -8,22 +8,21 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
 
-import nz.co.afleet.bit603_a3_johnmcpherson.databinding.FragmentHomeBinding;
+import nz.co.afleet.bit603_a3_johnmcpherson.databinding.FragmentUsersBinding;
 
-public class HomeFragment extends Fragment {
+public class UsersFragment extends Fragment {
 
-    private FragmentHomeBinding binding;
+    private FragmentUsersBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
-        binding = FragmentHomeBinding.inflate(inflater, container, false);
+        binding = FragmentUsersBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textHome;
-        textView.setText("This is the Home Fragment");
+        final TextView textView = binding.textUser;
+        textView.setText("This is the Users Fragment");
         return root;
     }
 
