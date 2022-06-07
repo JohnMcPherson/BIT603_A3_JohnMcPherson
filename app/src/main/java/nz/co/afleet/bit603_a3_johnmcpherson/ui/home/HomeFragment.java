46650ea454da818c893e367c3319da27c7df6abf
JohnMcPherson <john.mcpherson@xtra.co.nz>
2022-06-08 11:47:01 +1200

    Deleted TestHomeFragment (can't get User, so can't test the welcome message)
    HomeFragment generates a correct welcome message

--- app/src/main/java/nz/co/afleet/bit603_a3_johnmcpherson/ui/home/HomeFragment.java
+++ app/src/main/java/nz/co/afleet/bit603_a3_johnmcpherson/ui/home/HomeFragment.java
@@ -1,3 +1,10 @@
+/*
+ASSUMPTIONS
+    -   we are doing this for the same client, so I assume the client will want this screen to look similar
+        to the equivalent screen we built for assignment 2
+        (But, there is no requirement for favourite colour, so I have not replicated that)
+*/
+
 package nz.co.afleet.bit603_a3_johnmcpherson.ui.home;
 
 import android.os.Bundle;
@@ -9,6 +16,8 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import nz.co.afleet.bit603_a3_johnmcpherson.R;
+import nz.co.afleet.bit603_a3_johnmcpherson.database.User;
 import nz.co.afleet.bit603_a3_johnmcpherson.databinding.FragmentHomeBinding;
 
 public class HomeFragment extends Fragment {
@@ -20,6 +29,7 @@ public class HomeFragment extends Fragment {
 
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+        setWelcomeMessage();
 
         return root;
     }
@@ -28,5 +38,17 @@ public class HomeFragment extends Fragment {
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+    }
+
+    private void setWelcomeMessage() {
+        User loggedInUser = User.getLoggedInUser();
+        if (loggedInUser != null) { // We shouldn't be here without a logged in user, but just in case
+
+            // set the welcome message text
+            String stringWelcomeMessage = getContext().getString(R.string.welcome_message_header) + " " + loggedInUser.getName();
+            binding.textViewWelcomeMessage.setText(stringWelcomeMessage);
+
+            // no requirement for text colour
+        }
     }
 }
